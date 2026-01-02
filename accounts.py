b389ac002c6e03524c00a429d45edeb6a69d2a46
h78man99-a11y <h78man99@gmail.com>
2026-01-02 11:35:56 +0530

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- accounts.py
+++ accounts.py
@@ -3,16 +3,6 @@
 
 ACCOUNTS = [
 
-   {
-        "NAME": "cashmafia",
-        "JSON_URL": "https://gist.githubusercontent.com/h78man99-a11y/3c4535b3282d4f9ad39cee1a7ecac52c/raw/fafea4ac41c571b238246579afb2115e50a4c2ea/Mafia.json",
-        "FIREBASE_KEY": "AIzaSyCxoB5Kk1AEQt5c6CHsC-XVdNaxr4nu8Es",
-        "PROJECT_ID": "cash-mafia-5ae03",
-        "REFRESH_TOKEN": "AMf-vBz15OI6d0jzHVC_dNBgV0JxooHVNSh_qCxpCpDOhU5gJYCNzsrwoipTzoW0wtvMH_GUjonOcFP7_ykAjYTd6v5ZSrCK9ht8tay2F-magpLmckQlb2-XgOUqOPMUnRa5Oqjuoz6l2koC4NEhy63fOSAcyUNwqSV00_rifSf89IwKPrvyUC8ETUGHZ_siisKDBUfnudMV8kWXeig7ct8mxve-PogdugG8GsYOUVkr-cAahKWSikrRGyBCxJG8bugVwMwS8CSXGWNNuEH-MPn45qvp0zaEdxVmUKRgkcF__SNlVJ2mJotxErW45biWUj9trt23_KuhoQdGSVqY0YTXYROsLUFqX11wBHcQW0LCiTp9BN6W3BudvBQj5FEzDYmWYtOZN_BQfoWdkLTINgk8lXGokYrvXGHFe3YlReD8MJ7iLIa1PLI",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2099492"
-    },
-
     {
         "NAME": "Puzzle",
         "JSON_URL": "https://gist.githubusercontent.com/h78man99-a11y/ed0507c8bab25b31e944c6799a813f43/raw/3cfe4ccc25a495cc75a9a457658e4c21f797fc63/Puzzle.json",
@@ -25,12 +15,12 @@ ACCOUNTS = [
     
         {
         "NAME": "Panda",
-        "JSON_URL": "https://gist.githubusercontent.com/h78man99-a11y/0c6d9acab470d18f41a3b6fc502f4d1b/raw/eadd9cd4de46addd1a0c647ce284aa7202a36796/Panda.json",
+        "JSON_URL": "https://gist.githubusercontent.com/h78man99-a11y/fd06a28d4055d01e969d380e62d8ffe8/raw/59393aa8b6e580b3eb105ab9dfaa58acd98b8666/Pana.json",
         "FIREBASE_KEY": "AIzaSyAMQu13Wg_7UnuwSstH6JKfh37VIEZ4bGg",
         "PROJECT_ID": "cash-panda-76893",
-        "REFRESH_TOKEN": "AMf-vBzbq1Nn6pfa2Pzwrwo43VU7xEWs4yBNx8UkRa6SDnbxSW6U5FckosQqAVOpR_zaVFt66JAuyA3BJ4dLNlaKoyDQA33L7fhjlADQF2cWJ1-9EVA461nj2dWJDFCqCUv-T9zFnM55jcaHaA0jzZ7s4ZP9IyP3z6LzAMhT_vGaIXEjENNhGEelNggx-HKKaYRW727xzg1KUc-ZXyBw5KGkCv33DqmOcj_DUS9fYEM6tXkJrnhoKZTtkqK7UAcHSTmRqJkRJafousrAP7-WQ-ErOhmlXoJuEXpMoeGKELfLN1I7QX6t0phVdADY8gWHeNvUT0lVq1PZ34rcorphNpHfBRQCZcYaVxEsjnrLpyX0MT25TnkMEh0rzsEhMis1z7UuIQtRtpCcDwByd4sC2YoV7kBqAISmMJMBN29EdKFvIdBsQuHhX80",
+        "REFRESH_TOKEN": "AMf-vBx8pekSVsMvaV5rhR1A94yvPUlvVu8k79duC222bOS4MRdGtrytDiucxtyMx5VoCJWfKZMIjxBarhTVgh1RTy7Zq_EEdFjOnrsDOkA5zpMwyP8I_z2Hw72Dr1kXLocXPPLQBRl5ruY8NuFgbsNdGFWlL58pcKD2WP7_yfGK0eUMAvjaigUEEtu2z2NKMJrQns7962v853GmrSa7y7CaosUfuYI745hmTNeiaJK2biB9SXM_3KkuCCb3tijS9T4BgxVEBP7sKTgXh_1O5yGmMcwBYwxdSA94q-tD51zIt8486DWfCJsk7Ql-o7E1XER92gRLnB-Kt41TW7BPECB7lV-jXON9goseKD_gTx8LP6X1XvNgEm8PN3B_tnOfJeTau228vXkbCPGvVCldGBQlKrARr4DUYy5BhsZwlufDHZfoy9Q3mXQ",
         "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2042804"
+        "SPOT_ID": "2759530"
     },
   
     {
@@ -43,16 +33,6 @@ ACCOUNTS = [
         "SPOT_ID": "2238157"
     },
    
-{ 
-        "NAME": "cash-Mfia", 
-        "JSON_URL": "https://gist.githubusercontent.com/h78man99-a11y/ff8a913ecef46209866172ec7a846c73/raw/a35a5b3eee65ea1f30058d1073ef2355dc275b52/Mfa.json", 
-        "FIREBASE_KEY": "AIzaSyCxoB5Kk1AEQt5c6CHsC-XVdNaxr4nu8Es", 
-        "PROJECT_ID": "cash-mafia-5ae03", 
-        "REFRESH_TOKEN": "AMf-vBzZNebAKmPLD5png9VQ5HgTphMf_Wx60zOiE4Lghlyyg8L6SjbZRegVuRMvVxzeydLDEmXSOmvwRriVdTz7UD2wBFlVJD86C5-fQdktDWl4sQwA2aoM1lieNTxt1T4zoUNPUjEYywuUVbk286h6T5aSAAwOEbDu8WS-PG-daoO9KvETrH7f8ia0o7x0wgkx8XiW8BR-8GxL7KjydJZ7k5paA28OrbtCuv6uQDDBN1wJFyo9HDazP0y-01gLVa1huTzIUYgy0IX8xrCA82Nj7SuwkCtY2-JcZlSf26vWJ-2eZ7ykVHixmz5UHVGNJ6EqJAuAaQqUo6I-V2ucxjJKb2NFOhdgZD20v91tL3Q-UjTZbLxIAx8CogOHNW9VcjRr6orDoraG9zVUnaIjBvzLSN2zVYVHo39QmFvRJPdmRFRpRxT_LOI", 
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation", 
-        "SPOT_ID": "2099492" 
-    }, 
-    
     { 
         "NAME": "cashromeo", 
         "JSON_URL": "https://gist.githubusercontent.com/h78man99-a11y/6cbc001ddb6396e9adc2b2dd625bc12a/raw/89b5cdaed1a2f9ca6ef747d989e9f41993c5e5b9/Romeo.json", 
@@ -82,34 +62,7 @@ ACCOUNTS = [
         "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
         "SPOT_ID": "2555632"
     },
-
-    {
-        "NAME": "cashmafia",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/718e513f992b1c368ddc2f7e0182b6f5cbadafa4/Cm.json",
-        "FIREBASE_KEY": "AIzaSyCxoB5Kk1AEQt5c6CHsC-XVdNaxr4nu8Es",
-        "PROJECT_ID": "cash-mafia-5ae03",
-        "REFRESH_TOKEN": "AMf-vBys7wAAz7ed_vIj91n85qdeojzrOzHJ5ukt5_LienD1W_6KziHeX4utqw50Hc-zQrqCxE5SfVgtE6nHQKRtz9klGXcilmN3KB8uS75dmQ70DHzKuhqWZ2SWCqHoozKPM-hIXqdLDnEw4yqcqdonOfxaXZmdPYmICNS8NUE-Isd52pporQRP60C1Gy3tlmP2-aiz0m0OoDWPMJHuNmsELrORa7C6I2Xh9RHvgRdmi2S7RMVhzT-ByQQOzrfNce6idfOh_2HUPid15MiW7byRl486FQbdG2W3-sH1o5RpVAwMgFXZtuGOk4bmoXKdXqzL9D8D-iYHkM1uzHZNRQwZp3jnCJqHMuxv1kqbYJztBYZWxR_4oCQu5HkvLKrSbnflDGn6Yt6gK1leuEjRk2xnP6TjZGd8LC__inbo5h_cG_gJX-D7s2e_sng6DqupkOpp2aKHuU9g",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2099492"
-    },
-    {
-        "NAME": "cashpanda",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/87da6a7bb3a951a36cd65008405eaba4588af8ca/Clonecp.json",
-        "FIREBASE_KEY": "AIzaSyAMQu13Wg_7UnuwSstH6JKfh37VIEZ4bGg",
-        "PROJECT_ID": "cash-panda-76893",
-        "REFRESH_TOKEN": "AMf-vBx4Mhwk_CgLnSnHkJ5oZopwaE6h7upLFeXteZCQGCeJnfj98Dx4s7bBEa5BYzB76Ou23Rq37gjBQiSe_Bjo-SuJ9-flIIXxN39edfmcMYdUZrGzkVGr5lq4H4mpOl2pUfU09ZLho45AERjgVEK-d23i17NGpGzTkUgIOVqV05zYokjUa1-yAD0Jg4v_dHCVyaVxMA0Z-EIMGgpll_hQbHHpRThpA56D_H3dS0vjh8oOYasIqSuf7iwT44_TTP1ORN3GgNH9zraE_O42iOuw6nbEQVYDEUY3k4mBlrM8kXr7jipY43e3z5bPp0LaLjJcGj9EgnPZRbPU64ZEHOlNmDc4QaLwS3PEESAW_BroF3gRbJb4PpUmtMRoU9HgMycGYenKG1nPV09S256Z5DmKLqLmjUqLzVqf-sxfv_ra9wiROpPGhjk",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2398648"
-    },
-    {
-        "NAME": "cashpanda",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/33930d28abd20760646806bc1500724ad037091b/Manish.json",
-        "FIREBASE_KEY": "AIzaSyAMQu13Wg_7UnuwSstH6JKfh37VIEZ4bGg",
-        "PROJECT_ID": "cash-panda-76893",
-        "REFRESH_TOKEN": "AMf-vBwAFdYyt187xFEQRE754-WiH92q7hKiyqT3CdAzws9tl5d4AIg8U5BpSsWwvvtO69G6m4vRr_hA09i93yi_QONoakfds8FWOvYsuyRs5ge9UdnLOVy-n1ct8gGfQAf0DdPrL_Owecnlwg_HIEvFHislYDDLCHWhhtFxyTk1yppDqQCO7F1ssuwvnLRxYBSIYDy8VaFuqdTHf3ifL2Lfx_NP14wGrAceMi0aYOizWw0JSeg4Cl--IVhYHaNYZuQM_UsmMaA6-CJYVkyBgF9sZh8IU4_nfuEDV_Qtrd34HGr3TZ6Wy3TznOlIDfji3KTQjhhw7e-LxR_y2Snwu3jU4ZWxldORxbQ6p8NyWI-rr0zfar0p-GVPx69VVQoGuvEmVihI5RyElnWqSmJJ2LdcjC1TraSNqUA5dfbzuSpS-a5Q9qVg8sA",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2042803"
-    },
+    
     {
         "NAME": "cashpanda",
         "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/e6e8ea73c856b756fe5e67cd47782dcf53eb2f0c/Umesh.json",
@@ -119,69 +72,7 @@ ACCOUNTS = [
         "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
         "SPOT_ID": "2042803"
     },
-    {
-        "NAME": "cashpanda",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/804a524eed7e8f39a0a3923fcd7c83c562b087d7/Cash%20panda.json",
-        "FIREBASE_KEY": "AIzaSyAMQu13Wg_7UnuwSstH6JKfh37VIEZ4bGg",
-        "PROJECT_ID": "cash-panda-76893",
-        "REFRESH_TOKEN": "AMf-vBwAFdYyt187xFEQRE754-WiH92q7hKiyqT3CdAzws9tl5d4AIg8U5BpSsWwvvtO69G6m4vRr_hA09i93yi_QONoakfds8FWOvYsuyRs5ge9UdnLOVy-n1ct8gGfQAf0DdPrL_Owecnlwg_HIEvFHislYDDLCHWhhtFxyTk1yppDqQCO7F1ssuwvnLRxYBSIYDy8VaFuqdTHf3ifL2Lfx_NP14wGrAceMi0aYOizWw0JSeg4Cl--IVhYHaNYZuQM_UsmMaA6-CJYVkyBgF9sZh8IU4_nfuEDV_Qtrd34HGr3TZ6Wy3TznOlIDfji3KTQjhhw7e-LxR_y2Snwu3jU4ZWxldORxbQ6p8NyWI-rr0zfar0p-GVPx69VVQoGuvEmVihI5RyElnWqSmJJ2LdcjC1TraSNqUA5dfbzuSpS-a5Q9qVg8sA",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2042803"
-    },
-    {
-        "NAME": "tapandearn",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/e53d37e8310d972269235ecbff7487bee14b8166/Tapearn.json",
-        "FIREBASE_KEY": "AIzaSyAkSwrPZkuDYUGWU65NAVtbidYzE5ydIJ4",
-        "PROJECT_ID": "cash-rhino",
-        "REFRESH_TOKEN": "AMf-vBxY7NS_TNIkdoNKZX5Vwg94gNDP_f0XnHZfV84sylDiDKai4yDb8izRTgv1Hb5-5EiVw8KziUSk4p97iWtNAIJwJyaK4YWoibfU0iMOqB1WTUs4_rS0MFN8Pe4QjLd1C37Dike3oqkwptrXtoTPNqVmiQjLwA1tYLgVlAoz-QYwVbljRoOXz7iB_KY-e6rln4a5M7Tm8_-fVPMuB074bbNmOb5gmSNKl6VqC54epat1_ghoZd62Q5M8QbbLGhKQx6yRi-yxU_UIHihw_OFni5Q-TQt_eDKnro9mjtYYEXKtKzshl218k05lNkwZn5ovKtNg4mrBXj4M4j9N0LuQZi4BHaWxOWrkxWT9kW_e4DqdRqw9EZY2-Yx0XirFOGc9kq4ljg--rVI3CXd6m3AAAZAl7Wvsjg",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2238157"
-    },
-    {
-        "NAME": "cashthug",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/9081d1f5cde0f99fc1d48a7d22130f82bcd9d5ca/Cashthug.json",
-        "FIREBASE_KEY": "AIzaSyDArVb852ZEA9s4bV9NozW0-lVmX1UtsIg",
-        "PROJECT_ID": "quiz-cash-d2b1f",
-        "REFRESH_TOKEN": "AMf-vByxrGwfVXye3s86d9NxaBmqId3VAs_9_afW1yDUsU8_MBkO3lSaqvGMBo68M94F1Szp1r0ALoYO4iF4MS_R1q7AgeWD5y5bQbAITso6tDnJkO7W0NG35vQc-8wjKylNzfZHjWUFCTakSGypuPkbDoiJGduZQwdcgkQaGuj9ZvznIxvf8FEfk7msInP67s3KY1j3878dA01SKSm7SvZ9jHbRReZ9mnmhV7-pFqo6kHccE47V5b4AdxZ8c_vh-SQq20I6SZfQVhv_P30vlfXOTLwZkYCQx9CDg8X0VffoGrqMRXaMYSi16mDdmBtzlIlZZ6YSBvpc-hque1thgiL7A07TD49hWwx08vL6eMX2Qah7V3zRN-__hdnfgTz6Ov62l-KgRrGge3shsQ1e8Hbi6FghFHYCfuylv2MJGl4w-xnb232rj2R_G0P-viDY4OUfce9JGqof",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2238092"
-    },
-    {
-        "NAME": "cashthug",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/90976de9d44eb77dbf3e03ca6cc0033ee12e44de/Ctclone1.json",
-        "FIREBASE_KEY": "AIzaSyDArVb852ZEA9s4bV9NozW0-lVmX1UtsIg",
-        "PROJECT_ID": "quiz-cash-d2b1f",
-        "REFRESH_TOKEN": "AMf-vBxzA0MVzJkYuhxQ52pslFS9S7JZAoVtuC2ISxH-fKtBpQNvuFDsDTmdhwsGqiiLDioF6PpxhyspJZR00YLc2lVmMAS50yj1RmbJPfyxTjNOWKlSfcsUvjG9u6L47YQH-ihX8QM3QlOA8RuzIdgzBrnXQTMIT0WeEBPYAUqvTmrqjnFXdaboaBgQ3OaTbe0MVroFvkdsCUU_a_n7ZFvwV5XfID70CiKwJhVsn237yaWvcC_t7KCvHBWQv-exX7Pf4usdU-Tby2qI6uZ6xAENVjbTES884qIbt4bTIvZlTlJLs3uvntD0H0jUImqXPucI8fLDaGMjZJvBpUKljOJUzm4Ah4bDAcELMYWsxPLYH4ogcsb9JFsXLopSTvSKyfWGfQPoko5XRK0xnEbM49j30sjWz6tnTmHgTCEodAoHau26wMd-X3k",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2238092"
-    },
-    {
-        "NAME": "cashromeo",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/c024842a914f0a6e2b5871b24360ac62ff517a14/Cr.json",
-        "FIREBASE_KEY": "AIzaSyBBK7rf67OjPHTEVNRWjFglPMKTF4Ze2kQ",
-        "PROJECT_ID": "cash-romeo",
-        "REFRESH_TOKEN": "AMf-vBwAUlzIS5O_jLObBwuIHtaLe0_2jZ76Q4WeJa-PdvC8QX5SKmjU23ukMgHKHE-7D33xeOIux67oahBZt0z12jHQ9NCS-6E5QJL5LfxdVIxu2GhcIwL-eaVWCew-G4ja-RFjicgZlerWrA6wRg-SacN_WFpve5ZCYu9lMhMOsR1cwN5J-XXuxD1zX2wSDfEdlV30zVTHgxNvX1zvMM2IBc_pec2V3guHhFOT1z8445inapD_d_c9DLORdnSI5u-o64AwmfeD4BAVMAYY03ei94mx3xUeyY3tHVTp3jWsKZdqQoLdNvxYzcozYRKn-XAkdn_tjaA__3W1Rs1QE-UvWgJJdwQsJpBWWQeqZyu5SDA-0ul8NpmcN26nmBFSaphAfcWWT33xU9jFky0P9mKVt9ZkXf07vK0_c8VVD5h9oteW_ZFUYpc",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2369786"
-    },
-    {
-        "NAME": "casromeo",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/59e38e432e83415d1877e6a0cf098f5142e60558/Cr1clone.json",
-        "FIREBASE_KEY": "AIzaSyBBK7rf67OjPHTEVNRWjFglPMKTF4Ze2kQ",
-        "PROJECT_ID": "cash-romeo",
-        "REFRESH_TOKEN": "AMf-vBxHMi7BwpRoXSMch5oTq1snoLOURRI3oDNV7-lNuEoYtG4zY1napQ3xhfY_NUt4dqJIi8AnKkhXdtSkSg_YD0y9_TjY7xvnDOf76w-Go4brHNVUax7VoRcG3Fpj2byPZlJOnMbSpfurgMD4n7BALDftJ-EBkcDIm0mW-BoBc_2M4kTSnCZhRR_Dr21fBxWwDK5qVzb53gyJcY17Prn1TZ-Vpe_UI6gjndW8RiYCwNF6c6VKmuZz5HItwjBPYnLbZqaEqBub-JZBLK4SunYVs8odKEm8ER2LsnjsBcr3XX3JGReUQ2Xlsbcs7p4o5xVPLXuLob9NBRg3OGo-VtOA_PufhJUQFKKbtdhTnpz1uZsJQh9M1YFjJpdL2-qgZTdcBwvewDJ1EwFITpn-FN_w4qn5-Q0QbmY5Oju3X31hGpLGvrIIbWg",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2369785"
-    },
-    {
-        "NAME": "cashmafia",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/a2adc6d4d21bec68705ac7d27cab487ba1bf0385/Cm1clone.json",
-        "FIREBASE_KEY": "AIzaSyCxoB5Kk1AEQt5c6CHsC-XVdNaxr4nu8Es",
-        "PROJECT_ID": "cash-mafia-5ae03",
-        "REFRESH_TOKEN": "AMf-vBy5Nep841ToBgIaSLFwNxWGZK193lnm6c3lGX2DQ1TCF_P3J-SUCyVlol4VmJg2G3G6jEQYRy_1A8gb2LoaM_1uM3An8ZGlfQW1QQuML3Cc2L-tEeWoJwa-D1hltbIiKDc-jV86JvEV1gAgkUIvNy1e2ZGnQ_sWENVA7bpXbBB3Wkv8LZsHasAU4v96i7w4W6QZq5R2ekYFnjZ5xWyyiM7cueGrvU3WMvqtZmGMVQN9GXRceTesFNzFO96lTMnKhjY-La7wec1jeH8d0-40aCbKvOYN7dPUeB7hXPTo8cZj41QT7D99MeDASlFzDbgk3NpC3DqQt2RfAImwV6TYSxzjyUZAB3CN2IO1f7AGy9nXJFl62XOFDO355A_6BQJ18sGXZbk9YVdJnOE9TFMUwiNCFfPy5-Fftn4lgP3Qz1Gw5soxzOU",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2099492"
-    },
+   
     {
         "NAME": "tapandearn",
         "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/fea75540f5aecb18aca8ee768d94735e46dfee1f/Tp1clone.json",
@@ -191,42 +82,7 @@ ACCOUNTS = [
         "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
         "SPOT_ID": "2238157"
     },
-    {
-        "NAME": "cashthug",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/82495b091101d7cdacfddc34c8a84e342c5296c1/Ctclone2.json",
-        "FIREBASE_KEY": "AIzaSyDArVb852ZEA9s4bV9NozW0-lVmX1UtsIg",
-        "PROJECT_ID": "quiz-cash-d2b1f",
-        "REFRESH_TOKEN": "AMf-vBxXrBRW0NfkJBxaJbUMmuccYrpzkNShhlQQVWLPv0r2lfQbgtUHhPXzoyzqAcuFBG9cgrVgXbFRXy7xBmlav1i-t02G0oKZoniwLA7l01LlIMgDNwYu8EAcs0ggk-v8S_E_kS9-iorRAGGzcAroEdU-61gH3VRzjFDcKTZRUinEhEfp_LWA3b1UF-BiwXClQr2eGRwjQbe7gISHTW6T5Z2PcrlHxOydBrLYTNEwokmXDofkHVR7L8dkiwhtIAaKj16fhdwvBSTxM0xUykQfNJRXUqZzxKGZyo5jcUzX1Xq8fgVYMLmyPX3W6AQDLN7RP5RbNxq3SchblFhTjlqqFO-ZDVkuE4ivRj1to6Cwb_Jk8bDlEH0Ro1M_sZtKr1qDH4X_2vyBKwcYOw8CytftzKJASDAMKUeB-5Xsf2uKs1ZMMjKvjTk",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2238090"
-    },
-    {
-        "NAME": "cashromeo",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/93d9a07107da6b74cab62d52c5c7a201a2615075/Cr2clone.json",
-        "FIREBASE_KEY": "AIzaSyBBK7rf67OjPHTEVNRWjFglPMKTF4Ze2kQ",
-        "PROJECT_ID": "cash-romeo",
-        "REFRESH_TOKEN": "AMf-vBxdmNMRv7hEv33xKvrTzMZS2Sa3EBDgcdnwKBrOv9_nTB-_1qypFsAaQj-jf_OjYE1JnTy4zGpY2t25ANCrBeP2ucFAguWktKdQl6ficlsvIwhOrkmjOFrPHYyV0tu2SDenumy28DPcehZVIxKncz1btO54XE-spOv8vvROi8VcBUssEKAiEZuTTgMWOdW6tZ14RcBYIgHFYs91Oyy5U2HgDA9QgUboEj5aFvnTrOcnKHpMdSnBu7Nwb9jsOYTLrIHwgeKn6tkEW7d_YcEqqs87e0vmdAyOie4jIuWDSqbnLyOXlukCtDvRA0ejPud_Rz-UjXHaEbZFEwTdCZP9BLLqvz0ObKXT27ljMYcVIXkDIFJSazV4-OAaNCF40ocWybDdGw8JdjdSE7XOIoPePlY5Fpe7Sw",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2369786"
-    },
-    {
-        "NAME": "cashmafia",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/56e9d5be16e78e0d011d317038f062c5ecb67428/Cm2clone.json",
-        "FIREBASE_KEY": "AIzaSyCxoB5Kk1AEQt5c6CHsC-XVdNaxr4nu8Es",
-        "PROJECT_ID": "cash-mafia-5ae03",
-        "REFRESH_TOKEN": "AMf-vBx-2xUaYA41g90dpAFOC98Lt_HNmywoMzMS3Nj4Kl4dCgbSmW9QVruigNNMWIFZGr2yNeA1av9GGSinQjnh0ZvuWHlHMMXplizNcW3hUjxzrTi99PWPQP0v3-R39vqXvjzQAJxX5SMlt-i-TkHfKEXocQM89mZtbXk1ypStywwyHr60KcTWHH2F84zri4MZZzYWQU4YHWhxf-IbUEuy4WrtbVRuUM4ZpEtN7lFP_o37TGfQAdg7XbpyIgyUYCKUp1jAihKdQN_nujzZ1JLjlzav1s2s3Ppj8fVm0K7sOLM3Po9MKiLz0U_Ny0fboCP5_EKf00JZ4Do423TMqiaLx8BuTIzX2rTRAHfGI-_UjFuZGDl1F9cEiEqnZt2Z9p6SCzUZVRMdm6AHac9--M1GPEx0NMnjEwms03_smlOTHZLfVuRAMn4",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2099493"
-    },
-    {
-        "NAME": "cashpanda",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/45e09960e59145df67a4631988ae755a88369bc0/Cpclone2.json",
-        "FIREBASE_KEY": "AIzaSyAMQu13Wg_7UnuwSstH6JKfh37VIEZ4bGg",
-        "PROJECT_ID": "cash-panda-76893",
-        "REFRESH_TOKEN": "AMf-vBwALQoPkQKMgJOYuicHk-ONyo1xt4Yav-xxcQen1HjnduzZpNxhGK2C2UWyx6xCzEfAFEVbfQzmK5G94chBrqA1dv4pzZ05iGibQ7OaMMjYD84Bf1Ykdf8I95eGwLfVJFa95bIJnqcS8DBUqnuhoVieUIl7ZG6tI4fNS-pl8e9mm7fKTEfmb7b-HIIXBBImPjJkYp2hSy3sCpZa7gz2y8c3lWCkDHOImLz21ZBgb0kzYinS500JKmwJb3agCydM9RxAa2cof5lqxPZRFpdLVEhsxYm1bmJPjx-EgHAwULiDoOSEff5LiYPEaiqlluAYA_GE3ylz2OEvWLPUzZ1qQNmERJb6UCZn8mvnP5Hos3oRtIbcctuZ4_TlBDqpq6P_lIEORZ8HjQy7MCAzlOWW75dZbmOyLCLYVsRmMbzoBSdCM7Hc4Oc",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2042804"
-    },
+   
     {
         "NAME": "tapearn",
         "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/5b0c5bd205bc8df5d1838d68336122e2b4f7cf33/Tp2clone.json",
@@ -236,14 +92,4 @@ ACCOUNTS = [
         "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
         "SPOT_ID": "2238156"
     },
-    {
-        "NAME": "cashpanda",
-        "JSON_URL": "https://raw.githubusercontent.com/rajukushwaha7116-eng/Checking/44dbfb87aed0225627da8b91fea889168e11fff9/Cp3clone.json",
-        "FIREBASE_KEY": "AIzaSyAMQu13Wg_7UnuwSstH6JKfh37VIEZ4bGg",
-        "PROJECT_ID": "cash-panda-76893",
-        "REFRESH_TOKEN": "AMf-vBwT66scizjlRfwuueIllouKs07UKmOm0oQC3MR9Mf-aeBt9SpcbjSOArTiFEHDLqS5nqOfuHZ8Z_AIXuDlzdf8GURsNi4mJ5HEfm_slEEAvy89zu4nDC2M_H6xuXlwf-5GoFD8jvVB1wycB4RSz3t0uJWT9wWv-IuCm3WGY1i7Tq6aQqWu-rpFyEDLbvyzVJajS4wzoVuOH0VUSief8I1V99Kwo-PlROk54zQqRFj_2sTVXmK5IXxAY2mJoKEjRogEQRmSzPGujYkZOowpP0bzpieli5UujHdLIdrNU7An_IqmpUumcjWPS7IyUz9rvSE2M2ZoGJwPuzCHAdd6zXz7nLpWnZDVhrhHR-gA7HJUr7ztLejM3q0FlFRV6hIjWys4xdhSFO2pTRXPzeCm3pKbkZgMHGpWXQNVmPRSNNOP3I0nValg",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2042803"
-    },
 ]
-
